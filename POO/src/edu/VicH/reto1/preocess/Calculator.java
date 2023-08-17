ea710a2f446e2029ab41a57c0ae6102ff96dc87c
Ferrokanon <victorssshugo@gmail.com>
2023-08-17 10:07:19 -0600

    Reto1

---
+++ POO/src/edu/VicH/reto1/preocess/Calculator.java
@@ -0,0 +1,61 @@
+package edu.VicH.reto1.preocess;
+
+import edu.VicH.reto1.ui.CLI;
+
+/**
+ * @Author Victor Marintez
+ * Esta calse contiene la logica de los operadores aritmeticos basicos:
+ * suma, resta, multiplicacion, division y modulo
+ **/
+public class Calculator {
+    /**
+     *
+      * @param num1  primer operando de la suma
+     * @param num2 segundo operando de la suma
+     * @return resultado de la suma de num1 y num2
+     *
+     * este metodo sirve para sumar 2 numeros enteros
+     */
+
+    public static int sumaEnteros(int num1, int num2){
+        return num1 + num2;
+    }
+    /**
+     *
+     * @param num1  primer operando de la resta
+     * @param num2 segundo operando de la resta
+     * @return resultado de la resta de num1 y num2
+     *
+     * este metodo sirve para restar 2 numeros enteros
+     */
+
+    public static int restaEnteros(int num1, int num2){
+            return num1 - num2;
+    }
+
+
+
+    public static int multiplicacionEnteros(int num1, int num2){
+        return num1 * num2;
+    }
+
+
+
+    public static int divisionEnteros(int num1, int num2){
+        return num1 / num2;
+    }
+
+
+
+    public static int moduloEnteros(int num1, int num2){
+        return num1 % num2;
+    }
+
+
+
+
+
+
+
+    }
+
