26354d4ad1305518ac2496387ea304f8e7a62c77
Ferrokanon <victorssshugo@gmail.com>
2023-08-22 10:08:54 -0600

    Reto1
    Hotfix de los cambios realizados

--- POO/src/edu/VicH/reto1/ui/CLI.java
+++ POO/src/edu/VicH/reto1/ui/CLI.java
@@ -1,13 +1,17 @@
 package edu.VicH.reto1.ui;
 
+import edu.VicH.reto1.preocess.Calculator;
+
 import java.util.Random;
+import java.util.Scanner;
 
 public class CLI {
     private static Random scanner;
 
     public static void showMenu() {
 
-        Scanner;
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("SELECCIONA LA OPERACIÓN DESEADA:");
         System.out.println("1. Suma:");
         System.out.println("2. Resta:");
@@ -18,7 +22,36 @@ public class CLI {
 
         int opcionMenu = scanner.nextInt();
 
+        if (opcionMenu >= 1 && opcionMenu <= 5) {
+            System.out.println("Ingrese el primer número: ");
+            int num1 = scanner.nextInt(); // Read the first number
+            System.out.println("Ingrese el segundo número: ");
+            int num2 = scanner.nextInt(); // Read the second number
+
+            switch (opcionMenu) {
+                case 1:
+                    System.out.println(num1 + "+" + num2 + "=" + Calculator.sumaEnteros(num1, num2));
+                    break;
+                case 2:
+                    System.out.println(num1 + "-" + num2 + "=" + Calculator.restaEnteros(num1, num2));
+                    break;
+                case 3:
+                    System.out.println(num1 + "*" + num2 + "=" + Calculator.multiplicacionEnteros(num1, num2));
+                    break;
+                case 4:
+                    System.out.println(num1 + "/" + num2 + "=" + Calculator.divisionEnteros(num1, num2));
+                    break;
+                case 5:
+                    System.out.println(num1 + "%" + num2 + "=" + Calculator.moduloEnteros(num1, num2));
+                    break;
+            }
+        } else if (opcionMenu == 6) {
+            System.out.println("Saliendo del programa.");
+        } else {
+            System.out.println("Opción inválida.");
+        }
+
 
     }
-
 }
+
