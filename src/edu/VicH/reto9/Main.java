e27dab18ed73d136fbd738fc4752d94e335e405c
Ferrokanon <victorssshugo@gmail.com>
2023-10-21 13:39:31 -0600

    Actualizacion 4.5:
    reto9---Uso de constructores y sentencia Override (anular)

--- src/edu/VicH/reto9/Main.java
+++ src/edu/VicH/reto9/Main.java
@@ -1,32 +1,16 @@
 package edu.VicH.reto9;
 
-
 import edu.VicH.reto9.process.Ayudante;
-import edu.VicH.reto9.process.Ayudante.AyudanteEspecializado;
+import edu.VicH.reto9.ui.Cli;
 
-import java.util.ArrayList;
 import java.util.List;
 
-    public class Main {
-        public static void main(String[] args) {
-            List<Ayudante> listaAyudantes = new ArrayList<>();
+public class Main {
+    public static void main(String[] args) {
+        // Genera una lista de 5 Ayudantes aleatorios utilizando la interfaz Cli
+        List<Ayudante> listaAyudantes = Cli.generarAyudantesAleatorios(5);
 
-            for (int i = 0; i < 5; i++) {
-                // Generar un ayudante regular o especializado aleatoriamente
-                double randomValue = Math.random();
-                if (randomValue < 0.5) {
-                    listaAyudantes.add(new Ayudante());  // Generar un ayudante regular
-                } else {
-                    listaAyudantes.add(new AyudanteEspecializado());  // Generar un ayudante especializado
-                }
-            }
-
-            for (int i = 0; i < listaAyudantes.size(); i++) {
-                // Imprime los detalles de cada ayudante en la lista
-                System.out.println("Ayudante " + (i + 1) + ": " + listaAyudantes.get(i));
-            }
-        }
+        // Imprime los detalles de los Ayudantes utilizando la interfaz Cli
+        Cli.imprimirDetallesAyudantes(listaAyudantes);
     }
-
-
-
+}
--- src/edu/VicH/reto9/ui/Cli.java
+++ src/edu/VicH/reto9/ui/Cli.java
@@ -1,29 +1,56 @@
 package edu.VicH.reto9.ui;
 
-
 import edu.VicH.reto9.process.Ayudante;
 import edu.VicH.reto9.process.Ayudante.AyudanteEspecializado;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Esta clase representa la interfaz de línea de comandos (CLI) para un juego con Ayudantes.
+ */
 public class Cli {
-    public static void main(String[] args) {
+
+    /**
+     * Este método crea una lista de Ayudantes aleatorios (regulares o especializados) y la devuelve.
+     *
+     * @param cantidad La cantidad de Ayudantes a crear.
+     * @return Una lista de Ayudantes generados aleatoriamente.
+     */
+    public static List<Ayudante> generarAyudantesAleatorios(int cantidad) {
         List<Ayudante> listaAyudantes = new ArrayList<>();
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < cantidad; i++) {
+            Ayudante nuevoAyudante;
             // Generar un ayudante regular o especializado aleatoriamente
             double randomValue = Math.random();
             if (randomValue < 0.5) {
-                listaAyudantes.add(new Ayudante());  // Generar un ayudante regular
+                nuevoAyudante = new Ayudante();  // Generar un ayudante regular
             } else {
-                listaAyudantes.add(new AyudanteEspecializado());  // Generar un ayudante especializado
+                nuevoAyudante = new AyudanteEspecializado();  // Generar un ayudante especializado
             }
+            listaAyudantes.add(nuevoAyudante);
         }
 
+        return listaAyudantes;
+    }
+
+    /**
+     * Este método imprime los detalles de los Ayudantes en la lista.
+     *
+     * @param listaAyudantes La lista de Ayudantes cuyos detalles se imprimirán.
+     */
+    public static void imprimirDetallesAyudantes(List<Ayudante> listaAyudantes) {
         for (int i = 0; i < listaAyudantes.size(); i++) {
             // Imprime los detalles de cada ayudante en la lista
             System.out.println("Ayudante " + (i + 1) + ": " + listaAyudantes.get(i));
         }
     }
+
+    public static void main(String[] args) {
+        List<Ayudante> listaAyudantes = generarAyudantesAleatorios(5);
+
+        imprimirDetallesAyudantes(listaAyudantes);
+    }
 }
+
